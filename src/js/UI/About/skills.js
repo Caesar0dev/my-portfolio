8a9b27b1397848e61416a2639c404264ab93d0f2
devboss0924 <smallstar0924@gmail.com>
2023-12-12 08:32:49 +0900

    initial deposite

---
+++ src/js/UI/About/skills.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        name: 'Laravel',
+        width: '100%',
+    },
+    {
+        name: 'React/NodeJS',
+        width: '100%',
+    },
+    {
+        name: 'Vue/NuxtJs',
+        width: '100%',
+    },
+    {
+        name: 'Scraping',
+        width: '100%',
+    },
+    {
+        name: 'Angular',
+        width: '90%'
+    },
+    {
+        name: 'Django',
+        width: '90%',
+    },
+    {
+        name: 'Wordpress',
+        width: '90%',
+    },
+]
---
+++ src/js/UI/Work/items.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    id: 0,
+    name: "Operate at a higher level",
+    description: "The leading virtual reality surgical training and assessment platform.",
+    image: "images/projects/osso-heco.jpg",
+    tags: ["VR", "React", "Nodejs", "TailwindCSS"],
+    liveview: "https://osso-heco.webflow.io/",
+    alt: "Hospital website portfolio project",
+  },
+  {
+    id: 1,
+    name: "CryptoMines",
+    description: "Mint NFTs, Explore Planets",
+    image: "images/projects/cryptomines.jpg",
+    tags: ["Web3", "React", "Nodejs", "TailwindCSS","Solidity"],
+    liveview: "https://cryptomines.app/",
+    alt: "Mint NFTs, Explore Planets portfolio project",
+  },
+  {
+    id: 2,
+    name: "Food Delivery",
+    description: "Food Delivery Groceries, shops, pharmacies, anything!",
+    image: "images/projects/glovoapp.jpg",
+    tags: ["Vue", "Nuxt", "Laravel", "TailwindCSS"],
+    liveview: "https://glovoapp.com/",
+    alt: "Food Delivery portfolio project",
+  },
+  {
+    id: 3,
+    name: "Portfolio 2022",
+    description: `My portfolio website, you're probably looking at right now.`,
+    image: "images/projects/portfolio.jpg",
+    tags: ["webgl", "React", "TailwindCSS"],
+    github: "https://github.com/devboss0924/my-portfolio.git",
+    alt: "david heckhoff portfolio project 2022 threejs blender 3d design",
+    bannerIcons: [
+      { src: "images/cssda-icon.png", alt: "cssda icon site of the day" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Drumelia LUXURY REAL ESTATE",
+    description:
+      "This is the Luxury Real Estate website",
+    liveview: "https://www.drumelia.com/",
+    image: "images/projects/drumelia.jpg",
+    tags: ["Django", "Mysql", "Vue", "TailwindCSS"],
+    alt: "This is the Luxury Real Estate website",
+  },
+  {
+    id: 5,
+    name: "Dexify Finance",
+    description:
+      "Dexify Finance",
+    liveview: "https://www.dexify.finance/",
+    image: "images/projects/dexify.jpg",
+    tags: ["Web3", "React", "TailwindCSS", "Nodejs"],
+    alt: "Dexify Finance",
+  },
+  {
+    id: 6,
+    name: "NFT Show Room",
+    description:
+      "NFT Show Room",
+    image: "images/projects/nftshowroom.jpg",
+    liveview: "https://nftshowroom.com/",
+    tags: ["Vue", "Nuxt", "Nodejs", "TailwindCSS"],
+    alt: "NFT Show Room",
+  },
+  
+];
